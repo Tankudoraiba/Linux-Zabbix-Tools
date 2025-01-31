f0a4d93afe07a05a90814a5d89a30bdfbc7d820a
PolishTanker <wojciech.borak@gmail.com>
2025-01-31 19:11:29 +0100

    .

--- Service Status/scripts/service_status/get_service_status.sh
+++ Service Status/scripts/service_status/get_service_status.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Get the service name passed as the first argument
 SERVICE_NAME="$1"
 
-# Replace "%" with "@" if it exists in the service name
 SERVICE_NAME=$(echo "$SERVICE_NAME" | sed 's/%/@/g')
 
-# Check if the service is running using systemctl or ps
 if systemctl is-active --quiet "$SERVICE_NAME"; then
-    # If the service is running, return 1
     echo 1
 else
-    # If the service is not running, return 0
     echo 0
 fi
 
--- Service Status/scripts/service_status/get_service_uptime.sh
+++ Service Status/scripts/service_status/get_service_uptime.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-# Get the service name passed as the first argument
 SERVICE_NAME="$1"
 
-# Replace "%" with "@" if it exists in the service name
 SERVICE_NAME=$(echo "$SERVICE_NAME" | sed 's/%/@/g')
 
-# Check if the service is running using systemctl
 if systemctl is-active --quiet "$SERVICE_NAME"; then
-    # Get the uptime of the service in seconds
-    UPTIME=$(systemctl show "$SERVICE_NAME" --property=ActiveEnterTimestamp | awk -F'=' '{print $2}')
     
-    # Convert the timestamp to seconds since epoch
+    UPTIME=$(systemctl show "$SERVICE_NAME" --property=ActiveEnterTimestamp | awk -F'=' '{print $2}')
+
     UPTIME_SECONDS=$(date --date="$UPTIME" +%s)
     
-    # Get the current time in seconds since epoch
     CURRENT_TIME=$(date +%s)
-    
-    # Calculate the difference (uptime in seconds)
+
     echo $((CURRENT_TIME - UPTIME_SECONDS))
 else
-    # If the service is not running, return 0
     echo 0
 fi
