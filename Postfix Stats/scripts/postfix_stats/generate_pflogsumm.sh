d92c610a1396e39d43ec9fdb020820038f5d9f74
Tankudoraiba <wojciech.borak@gmail.com>
2025-02-02 15:07:08 +0100

    change tmp folder

--- Postfix Stats/scripts/postfix_stats/generate_pflogsumm.sh
+++ Postfix Stats/scripts/postfix_stats/generate_pflogsumm.sh
@@ -6,4 +6,4 @@
 [ -f /var/log/postfix.log ] && MAILLOG=/var/log/postfix.log
 
 
-/usr/sbin/pflogsumm ${MAILLOG} -h 0 -u 0 --bounce_detail=0 --deferral_detail=0 --reject_detail=0 --no_no_msg_size --smtpd_warning_detail=0 -d today > /tmp/maillog_summary
+/usr/sbin/pflogsumm ${MAILLOG} -h 0 -u 0 --bounce_detail=0 --deferral_detail=0 --reject_detail=0 --no_no_msg_size --smtpd_warning_detail=0 -d today > /etc/zabbix/zabbix_agent2.d/scripts/postfix_stats/maillog_summary
