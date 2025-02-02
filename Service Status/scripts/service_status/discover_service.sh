56203feb5782ea7bec3bbcf4c2eaf12fb203c2f5
Tankudoraiba <wojciech.borak@gmail.com>
2025-02-02 16:16:15 +0100

    adding ports to service_status

--- Service Status/scripts/service_status/discover_service.sh
+++ Service Status/scripts/service_status/discover_service.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-OUTPUT_FILE="/etc/zabbix/zabbix_agent2.d/scripts/service_status/service_statuses.txt"
+OUTPUT_FILE="/etc/zabbix/zabbix_agent2.d/scripts/service_status/tmp/service_statuses.txt"
+OUTPUT_FILE2="/etc/zabbix/zabbix_agent2.d/scripts/service_status/tmp/port_statuses.txt"
 
 echo "" > $OUTPUT_FILE
+echo "" > $OUTPUT_FILE2
 
 systemctl list-units --type=service --state=running --all --no-pager --no-legend | awk '{print $1}' | while read SERVICE; do
 
-    SERVICE=$(echo "$SERVICE" | sed 's/@/%/g')
-
+    PID=$(systemctl show "$SERVICE" --property MainPID | cut -d'=' -f2)
+    PORT=$(sudo lsof -i -P -n | grep LISTEN | grep "$PID" | awk '{print $9}' | cut -d':' -f2)
+    SERVICE=$(echo "$SERVICE" | sed 's/@/%/g')    
+    
     echo "$SERVICE" >> $OUTPUT_FILE
+    echo "$PORT" >> $OUTPUT_FILE
 done
 
 echo "Service statuses saved to $OUTPUT_FILE"
