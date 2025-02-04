f8d791dc574962d2a746f68d7b50f15d5bffaec1
PolishTanker <wojciech.borak@gmail.com>
2025-02-04 11:32:51 +0100

    ports update

--- Service Status/scripts/service_status/discover_service.sh
+++ Service Status/scripts/service_status/discover_service.sh
@@ -4,7 +4,7 @@ OUTPUT_FILE="/etc/zabbix/zabbix_agent2.d/scripts/service_status/tmp/service_stat
 
 echo "" > $OUTPUT_FILE
 
-systemctl list-units --type=service --state=running --all --no-pager --no-legend | awk '{print $1}' | while read SERVICE; do
+/usr/bin/systemctl list-units --type=service --state=running --all --no-pager --no-legend | awk '{print $1}' | while read SERVICE; do
 
     SERVICE=$(echo "$SERVICE" | sed 's/@/%/g')    
     echo "$SERVICE" >> $OUTPUT_FILE
--- Service Status/scripts/service_status/discover_service_port.sh
+++ Service Status/scripts/service_status/discover_service_port.sh
@@ -4,44 +4,41 @@ OUTPUT_FILE="/etc/zabbix/zabbix_agent2.d/scripts/service_status/tmp/port_statuse
 
 echo "" > $OUTPUT_FILE
 
-systemctl list-units --type=service --state=running --all --no-pager --no-legend | awk '{print $1}' | while read SERVICE; do
+declare -A seen
+
+/usr/bin/systemctl list-units --type=service --state=running --all --no-pager --no-legend | awk '{print $1}' | while read SERVICE; do
 
     # Attempt to get MainPID using systemctl
-    PID=$(systemctl show "$SERVICE" --property MainPID | cut -d'=' -f2)
+    PID=$(/usr/bin/systemctl show "$SERVICE" --property MainPID | cut -d'=' -f2)
     
     SERVICE2=$(echo "$SERVICE" | cut -d'.' -f1)
     
     # If MainPID is empty or unavailable, try other methods
     if [ -z "$PID" ] || [ "$PID" -eq 0 ]; then
         # Use pgrep (finds the main process based on the service name)
-        PID=$(pgrep -o -f "$SERVICE2")
+        PID=$(/usr/bin/pgrep -o -f "$SERVICE2")
     fi
 
     # If still empty or 0, use ps to find the process
     if [ -z "$PID" ] || [ "$PID" -eq 0 ]; then
-        PID=$(ps -aux | grep "$SERVICE2" | grep -v grep | awk '{print $2}' | head -n 1)
+        PID=$(/usr/bin/ps -aux | grep "$SERVICE2" | grep -v grep | awk '{print $2}' | head -n 1)
     fi
 
     # If still 0, discard and print a message
     if [ -z "$PID" ] || [ "$PID" -eq 0 ]; then
         break
     else
-        PORTS=$(sudo lsof -i -P -n | awk -v pid="$PID" '$2 == pid && /LISTEN/ {print $9}' | cut -d':' -f2 | sort -n | uniq)
+        PORTS=$(/usr/bin/lsof -i -P -n | awk -v pid="$PID" '$2 == pid && /LISTEN/ {print $9 " " $8}' | sort -n | uniq)
     fi
 
-        PORT=""
-    for p in $PORTS; do
-        if [ -z "$PORT" ]; then
-            PORT="$p"
-        else
-            PORT="$PORT,$p"
+    while read -r line; do
+        PORT=$(echo "$line" | cut -d' ' -f1 | cut -d':' -f2)
+        TYPE=$(echo "$line" | awk '{print $2}')
+        KEY="$SERVICE,$TYPE,$PORT"
+        if [ -n "$PORT" ] && [ -n "$TYPE" ] && [ -z "${seen[$KEY]}" ]; then
+            echo "$KEY" >> $OUTPUT_FILE
+            seen[$KEY]=1
         fi
-    done
+    done <<< "$PORTS"
 
-    # Now you can use the final PORT variable
-    if [ -n "$PORT" ]; then
-        echo "$SERVICE,$PORT" >> $OUTPUT_FILE
-    fi
 done
-
-
