0022f0a24f784a4a39d972dfe85faeb58ba798ce
PolishTanker <wojciech.borak@gmail.com>
2025-02-18 13:07:47 +0100

    update templates

--- Postfix Stats/scripts/postfix_stats/generate_pflogsumm.sh
+++ Postfix Stats/scripts/postfix_stats/generate_pflogsumm.sh
@@ -2,8 +2,5 @@
 [ -f /var/log/mail.log ] && MAILLOG=/var/log/mail.log
 # For RHEL/Centos
 [ -f /var/log/maillog ] && MAILLOG=/var/log/maillog
-# For Others
-[ -f /var/log/postfix.log ] && MAILLOG=/var/log/postfix.log
 
-
-/usr/sbin/pflogsumm ${MAILLOG} -h 0 -u 0 --bounce_detail=0 --deferral_detail=0 --reject_detail=0 --no_no_msg_size --smtpd_warning_detail=0 -d today > /etc/zabbix/zabbix_agent2.d/scripts/postfix_stats/maillog_summary
+/usr/sbin/pflogsumm ${MAILLOG} -h 0 -u 0 --bounce_detail=0 --deferral_detail=0 --reject_detail=0 --no_no_msg_size --smtpd_warning_detail=0 -d today > /tmp/maillog_summary
